385f1dc5cd607e57b0274be5ecc8ff027f7e4359
Allan Guigal <allan.guigal0@gmail.com>
2022-02-15 10:50:37 +0100

    Add git ignore and progress on automate

--- Automate.cpp
+++ Automate.cpp
@@ -5,15 +5,22 @@
 #include "Automate.h"
 
 Automate::Automate(const string& expr, bool nombreNegatif) {
-
+    this->lexer = new Lexer(expr);
+    //statestack.push_back();
 }
 
 void Automate::decalage(Symbole *s, Etat *e) {
     symbolestack.push_back(s);
     statestack.push_back(e);
-
+    if (s->isTerminal()) {
+        lexer->Avancer();
+    }
 }
 
 void Automate::reduction(int n, Symbole *s) {
-
+    for (int i = 0; i < n; i++) {
+        delete(statestack.back());
+        statestack.pop_back();
+    }
+    statestack.back()->transition(*this, s);
 }
