1d55931b6ea830c130632be112cc8da2771d3708
Allan Guigal <allan.guigal0@gmail.com>
2022-02-15 08:49:11 +0100

    Init project

---
+++ cmake-build-debug/CMakeFiles/TPCompil.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TPCompil.dir/main.cpp.o"
+  "TPCompil"
+  "TPCompil.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TPCompil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
