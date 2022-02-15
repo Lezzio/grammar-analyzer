caa5800eebd5bcb3daac3db7bf4569806b18078e
Aurélien <5aurelientournade5+code@gmail.com>
2022-02-15 10:47:46 +0100

    Add Symbole.cpp

---
+++ Symbole.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <string>
+#include <map>
+using namespace std;
+
+enum Identificateurs { OPENPAR, CLOSEPAR, PLUS, MULT, INT, FIN, ERREUR, EXPR};
+
+const string Etiquettes[] = { "OPENPAR", "CLOSEPAR", "PLUS", "MULT", "INT", "FIN", "ERREUR" };
+
+class Symbole {
+   public:
+      Symbole(int i, bool t) : ident(i), terminal(t) {}
+      virtual ~Symbole() { }
+      void print();
+      bool isTerminal() {return terminal;}
+      operator int() const { return ident; }
+      virtual void Affiche();
+
+   protected:
+      int ident;
+      bool terminal;
+};
+
+
+class Expr : public Symbole {
+public:
+    Expr(): Symbole(EXPR, false){};
+    virtual ~ Expr() {};
+};
+
+class Entier : public Symbole {
+   public:
+      Entier(int valeur, bool t) : Symbole(INT,t), valeur(valeur) { }
+      ~Entier() { }
+      virtual void Affiche();
+      int getValeur() const {return valeur;}
+   protected:
+      int valeur;
+};
+
+
+class MultiplicationExpr : public Expr {
+public:
+    /**
+     * @param s1 SymbolWithValue *
+     * @param s2 SymbolWithValue *
+     * @return MultiplicationExpr with an evaluated value of s1 * s2.
+     */
+    static MultiplicationExpr *
+    newMultiplication(Entier *s1, Entier *s2) {
+        return new MultiplicationExpr(s1->getValeur() * s2->getValeur());
+    }
+
+private:
+    //explicit MultiplicationExpr(int valeur) : Entier(valeur) {}
+};
+
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "lexer.h"
+#include "Lexer.h"
 
 
 int main(void) {
